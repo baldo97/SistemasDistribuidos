e2b9317ba749a162699865f34d4dfb024ca92cd4
Sergio <marotosg@gmail.com>
2017-10-09 19:22:26 +0200

    Subo el código fuente de eventos y delegados

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class Program
+    {
+        public delegate void Print(int value);
+        public delegate int Sum(int x,int y);
+
+
+        static void Main(string[] args)
+        {
+            String x = "";
+            List<String> y = null;
+
+            Print printDelegate = PrintNumber;
+
+            printDelegate(500);
+            printDelegate(1000);
+
+            printDelegate = PrintMoney;
+
+            printDelegate(30);
+
+ 
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+
+        public static void PrintNumber(int value)
+        {
+            Console.Out.WriteLine("Print Number " + value);
+        }
+
+        public static void PrintMoney(int value)
+        {
+            Console.Out.WriteLine("Print Money " + value);
+        }
+
+
+        public static void PrintHelper(Print delegateFunc,int numToprint)
+        {
+            delegateFunc(numToprint);
+        }
+    }
+}
